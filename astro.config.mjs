68fa8d25d94a1fec01b1541a1819704f2c87f34f
Melani <coraline2719@icloud.com>
2024-08-17 17:05:12 +0200

    Initial work on Astro Starlight
    - Migrated to Astro Starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,28 @@
 import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Melani's Empire",
+      logo: {
+        src: "./src/assets/title3.png",
+        replacesTitle: true,
+      },
+      customCss: ["./src/styles/css.css", "@fontsource/lexend/400.css"],
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [{ label: "Example Guide", slug: "guides/example" }],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+});
